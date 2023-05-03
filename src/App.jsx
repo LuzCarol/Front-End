6218f294a9de6f3658c6aafc5654b633d4505095
LuzCarol <carolinaluz97@gmail.com>
2023-05-02 22:08:18 -0300

    novo trabalho

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div class="icones-headerall">
+          <div class="icones-header1">
+            <a href="perfil.html"><img src="icons/person-circle.svg" alt="Ícone do perfil" height="50" /></a>
+          </div>
+          <div class="icones-header2">
+            <a href="jogos.html"><img src="icons/bell-fill.svg" /></a>
+          </div>
+        </div>
+
+        <h1>Início</h1>
+      </header>
+      <div class="menu">
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="inicio.html">
+                <img src="icons/house-door-fill.svg" alt="house-door-fill" />
+              </a>
+            </li>
+            <li>
+              <a href="perfil.html">
+                <img src="icons/person-fill.svg" alt="person-fill" />
+              </a>
+            </li>
+            <li>
+              <a href="jogos.html">
+                <img src="icons/controller.svg" alt="controller" />
+              </a>
+            </li>
+            <li>
+              <a href="produtos.html">
+                <img src="icons/joystick.svg" alt="joystick" />
+              </a>
+            </li>
+            <li>
+              <a href="carrinho.html">
+                <img src="icons/cart3.svg" alt="cart3" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
