af4c854b53289c79d6464073509ab469b2e183f5
unknown <2214290069@academico.iesb.br>
2023-06-16 19:34:14 -0300

    Menu de navegação

---
+++ src/mainPerfil.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppJogos from './AppPerfil.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppJogos />
+  </React.StrictMode>,
+)
